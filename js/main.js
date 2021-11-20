dcf72505a912f7594be04bad09b73b20a118d1f0
MuhammadjonIsmoiljonov <muhammadjonismoiljonov0@gmail.com>
2021-11-20 23:52:10 +0500

    first coment

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+//  DOM elementlarini tanlab olish
+var cards=20;
+var pokimons = pokemons.slice(0, 20);
+ var elList = document.querySelector("#cards_list");
+ var elcontainer = document.querySelector("#quti");
+
+
+
+elList.innerHTML=null
+
+
+for (var item of pokimons) {
+  var newLi = document.createElement("li")
+  var newImg= document.createElement("img")
+  var newHeading = document.createElement("h5")
+  var newP= document.createElement("p")
+
+  newHeading.textContent = `Name: ${item["name"]} Type: ${item["type"]}`;
+  newImg.setAttribute("src",`${item["img"]}`);
+  newImg.setAttribute("width","300");
+  newLi.appendChild(newImg);
+  newLi.appendChild(newHeading);
+  newLi.appendChild(newP);
+  elList.appendChild(newLi);
+  elcontainer.appendChild(elList);
+
+newLi.classList.add("item");
+newHeading.classList.add("hed");
+newP.classList.add("tex");
+}
+
+
+
+
+
+
+
+
