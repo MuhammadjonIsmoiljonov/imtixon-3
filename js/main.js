43185e91c40a299cb203705c828f3d505597230b
MuhammadjonIsmoiljonov <muhammadjonismoiljonov0@gmail.com>
2021-12-23 15:41:47 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,36 +1,43 @@
 
 
 //  DOM elementlarini tanlab olish
-var cards=20;
-var pokimons = pokemons.slice(0, 20);
- var elList = document.querySelector("#cards_list");
- var elcontainer = document.querySelector("#quti");
+// var cards=20;
+// var pokimons = pokemons.slice(0, 20);
+//  var elList = document.querySelector("#cards_list");
+//  var elcontainer = document.querySelector("#quti");
 
 
 
-elList.innerHTML=null
+// elList.innerHTML=null
 
 
-for (var item of pokimons) {
-  var newLi = document.createElement("li")
-  var newImg= document.createElement("img")
-  var newHeading = document.createElement("h5")
-  var newP= document.createElement("p")
+// for (var item of pokimons) {
+//   var newLi = document.createElement("li")
+//   var newImg= document.createElement("img")
+//   var newHeading = document.createElement("h5")
+//   var newP= document.createElement("p")
 
-  newHeading.textContent = `Name: ${item["name"]} Type: ${item["type"]}`;
-  newImg.setAttribute("src",`${item["img"]}`);
-  newImg.setAttribute("width","300");
-  newLi.appendChild(newImg);
-  newLi.appendChild(newHeading);
-  newLi.appendChild(newP);
-  elList.appendChild(newLi);
-  elcontainer.appendChild(elList);
+//   newHeading.textContent = `Name: ${item["name"]} Type: ${item["type"]}`;
+//   newImg.setAttribute("src",`${item["img"]}`);
+//   newImg.setAttribute("width","300");
+//   newLi.appendChild(newImg);
+//   newLi.appendChild(newHeading);
+//   newLi.appendChild(newP);
+//   elList.appendChild(newLi);
+//   elcontainer.appendChild(elList);
 
-newLi.classList.add("item");
-newHeading.classList.add("hed");
-newP.classList.add("tex");
-}
+// newLi.classList.add("item");
+// newHeading.classList.add("hed");
+// newP.classList.add("tex");
+// }
 
+// ______________________________________________________________________________________________________
+
+
+    //  pokemons2
+
+var newPokemonList = pokemons.slice(o,20);
+console.log(newPokemonList);
 
 
 
